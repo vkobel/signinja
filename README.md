45dfd2549ca403ff028f215c084a3653ad0a423d
Vincent Kobel <v@kobl.one>
2021-05-14 18:47:15 +0200

    added static decorator

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Elliptic-curve toolset to extract crypto parameters from common formats (GPG, We
 Compatible with secp256r1/prime256v1/P-256 for now, supported next: Curve 25519 and secp256k1
 
 Supported: GPG and Webauthn (auth data, attestation, client data, ...) to OpenSSL ASN.1 DER structures.
+
+## Notes
+- GPG private keys have to be exported unencrypted (passphrases aren't supported)
--- signinja.py
+++ signinja.py
@@ -47,16 +47,19 @@ class BinaryExtractor:
 
 class OpenSSL:
 
+    @staticmethod
     def to_public_key(pubkey: ECPublicKey, alg: str = "prime256v1") -> bytes:
         if alg == "prime256v1" or alg == "secp256r1" or "p-256":
             return bytes.fromhex("3059301306072a8648ce3d020106082a8648ce3d03010703420004") + pubkey.x + pubkey.y
 
 
+    @staticmethod
     def to_private_key(privkey: ECPrivateKey, pubkey: ECPublicKey, alg: str = "prime256v1") -> bytes:
         if alg == "prime256v1" or alg == "secp256r1" or "p-256":
             return bytes.fromhex("30770201010420") + privkey.k + bytes.fromhex("a00a06082a8648ce3d030107a14403420004") + pubkey.x + pubkey.y
 
 
+    @staticmethod
     def to_signature(sig: ECSignature, alg: str = "prime256v1") -> bytes:
         if alg == "prime256v1" or alg == "secp256r1" or "p-256":
             r = (bytes.fromhex("022100") if sig.r[0] > 0x7f else bytes.fromhex("0220")) + sig.r
@@ -67,6 +70,7 @@ class OpenSSL:
 
 class CryptoExtractor:
 
+    @staticmethod
     def bytes_from_file(file_path: str, from_base64: bool = True) -> bytes:
         with open(file_path, "rb") as f:
             return b64decode(f.read()) if from_base64 else f.read()
@@ -74,6 +78,7 @@ class CryptoExtractor:
 
 class Webauthn(CryptoExtractor):
 
+    @staticmethod
     def extract_public_key(attestation_object: bytes = None, alg: str = "prime256v1",
                            from_file: str = None, from_base64: bool = True) -> ECPublicKey:
         if from_file:
@@ -90,7 +95,8 @@ class Webauthn(CryptoExtractor):
         else:
             raise NotImplementedError()
 
-
+    
+    @staticmethod
     def create_verifiable_message(authenticator_data: bytes = None, client_data: bytes = None,
                                   from_files: Tuple[str, ...] = None, from_base64: bool = True) -> bytes:
         if from_files:
@@ -106,6 +112,7 @@ class Webauthn(CryptoExtractor):
 
 class GPG(CryptoExtractor):
 
+    @staticmethod
     def extract_public_key(public_key: bytes = None, alg: str = "prime256v1",
                            from_file: str = None, from_base64: bool = False) -> ECPublicKey:
         if from_file:
@@ -119,6 +126,7 @@ class GPG(CryptoExtractor):
             return ECPublicKey(x, y)
 
 
+    @staticmethod
     def extract_private_key(private_key: bytes = None, alg: str = "prime256v1",
                             from_file: str = None, from_base64: bool = False) -> ECPrivateKey:
         if from_file:
@@ -132,6 +140,7 @@ class GPG(CryptoExtractor):
             return ECPrivateKey(k)
 
 
+    @staticmethod
     def extract_signature(signature: bytes = None, alg: str = "prime256v1",
                           from_file: str = None, from_base64: bool = False) -> ECSignature:
         if from_file:
@@ -145,6 +154,7 @@ class GPG(CryptoExtractor):
             return ECSignature(r, s)
 
 
+    @staticmethod
     def create_verifiable_message(message: bytes = None, gpg_sig: bytes = None,
                                   from_files: Tuple[str, ...] = None, from_base64: bool = False) -> bytes:
         if from_files:
@@ -155,7 +165,6 @@ class GPG(CryptoExtractor):
         gpg_sig_metadata = be.extract_from_pattern("8875", "x35")[0]
 
         return message + gpg_sig_metadata + bytes.fromhex("04ff000000") + bytes([len(gpg_sig_metadata)])
-
 
 
 if __name__ == "__main__":
@@ -179,7 +188,7 @@ if __name__ == "__main__":
     #sys.stdout.buffer.write(GPG.create_verifiable_message(from_files=("./gpg/msg.txt", "./gpg/sig.gpg")))
 
     # == GPG to OpenSSL private key
-    #gpg_private = GPG.extract_private_key(from_file="./gpg/r1.gpg.priv")
-    #ossl_private = OpenSSL.to_private_key(gpg_private, gpg_pubkey)
-    #sys.stdout.buffer.write(ossl_private)
-
+    gpg_pubkey = GPG.extract_public_key(from_file="./gpg/r1.gpg.pub")
+    gpg_private = GPG.extract_private_key(from_file="./gpg/r1.gpg.priv")
+    ossl_private = OpenSSL.to_private_key(gpg_private, gpg_pubkey)
+    sys.stdout.buffer.write(ossl_private)
